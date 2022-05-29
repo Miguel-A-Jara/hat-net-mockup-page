cfc5718b8861513e27549ae617115e6e25dddba9
Miguel-A-Jara <0miguel.jara15@gmail.com>
2022-05-29 13:59:44 -0400

    First commit

---
+++ src/MainApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavbarContainer from './components/Navbar/NavbarContainer';
+
+const MainApp = () => {
+  return (
+    <div className='bg-violet h-screen'>
+      <NavbarContainer />
+    </div>
+  );
+};
+
+export default MainApp;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        purple: '#6c63ff',
+        violet: '#b0a4da',
+        cream: '#fefedf',
+        'light-brown': '#d5cabd',
+      },
+    },
+  },
+  plugins: [],
+};
